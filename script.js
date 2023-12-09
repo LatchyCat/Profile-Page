eb42d7f33457c55632e2aa09e7e7a20d36b5cad6
oriel <orielsilvajr@gmail.com>
2023-12-09 06:39:12 -0500

    updated html & javaScript file to display date

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //Main container
 
+alert('Hello, World!');
+
+
+
+function displayDateTime() {
+    var currentDateTime = new Date();
+    var formattedDateTime = currentDateTime.toLocaleString();
+    document.getElementById("datetime-display").textContent = formattedDateTime;
+};
+
+window.onload = displayDateTime;
